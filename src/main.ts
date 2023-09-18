60fd37477c0a015fed04dc17caece5d1fcb5889f
Meidi Airouche <meidi.airouche@gmail.com>
2023-09-19 00:10:34 +0200

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*'],
+    origin: ['http://ec2-52-212-135-73.eu-west-1.compute.amazonaws.com'],
     methods: ['GET', 'POST'],
   });
   await app.listen(3000);
